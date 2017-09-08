4a9f5cadc9861984cc4062b592199bb99de442f5
dpaak2 <dpaak2@naver.com>
2017-09-08 14:41:59 +0900

    중간 커밋

---
+++ src/main/java/com/gms/web/member/MemberDAO.java
@@ -0,0 +1,18 @@
+package com.gms.web.member;
+import java.util.List;
+import java.util.Map;
+
+import com.gms.web.command.CommandDTO;
+
+
+public interface MemberDAO {
+	public String insertMember(Map<?, ?> map);
+	public List<?> selectAll(CommandDTO cmd);
+	public String count(CommandDTO cmd);
+	public StudentDTO selectById(CommandDTO cmd);
+	public List<?> selectByName(CommandDTO cmd);
+	public String updateProfile(MemberDTO bean);
+	public String deleteUser(CommandDTO cmd);
+	public MemberDTO login(CommandDTO cmd);
+
+}

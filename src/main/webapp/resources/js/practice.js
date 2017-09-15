995689ad8444bce90e6c0d387d04e46feecea7c4
dpaak2 <dpaak2@naver.com>
2017-09-15 17:11:40 +0900

    iife pattern @javascript

---
+++ src/main/webapp/resources/js/practice.js
@@ -0,0 +1,9 @@
+var practice=practice ||{};
+practice.common=(function() {
+	var init=function(){};
+	var onCreate=function(){};
+	var setContentView=function(){};
+	return {
+		init:init
+	};
+})();

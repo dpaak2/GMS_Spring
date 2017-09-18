f7c511dd1d748163065a9daf40712bbaeff7201c
dpaak2 <dpaak2@naver.com>
2017-09-18 18:55:58 +0900

    trash

--- src/main/webapp/resources/js/meta.js
+++ src/main/webapp/resources/js/meta.js
@@ -28,27 +28,51 @@ meta.common = (function(){
 /*
  * meta.index
  * */
-meta.index=(function(){   /*생성자*/
+meta.index=(function(){
 	/*instance variable*/
 	/*java의 필드에 넣는것과 동일하게 해야 한다 */
 	var $wrapper,ctx,img;
 	var init=function(){
 		onCreate();
+		meta.ui.init();
 	};
 	/*기능*/
 	var onCreate=function(){
 	
 		setContentView();
 		$wrapper=$('#wrapper');  
-		var cal=meta.algo.calculate().val();
-		/*eventHandler,  call back 함수이다! */
 		$('#button').on('click',function(){
 			alert('button click!!! yeah!!');
 			$wrapper.empty();
+			meta.ui.navbar();
 			meta.ui.arithmetic();
-			//meta.auth.init();
-			$wrapper.append(meta.algo.arithmetic());
-		
+			$('#arithBtn').on('click',function(){
+				alert('등차수열 클릭!');
+				$('#content').empty();
+				meta.ui.arithmetic();
+				$('#result').on('click',function(){
+					alert('결과');
+					$('#result_btn').text('결과 : '+ meta.algo.arithmetic($('#start').val(), $('#end').val()));
+				});
+				
+			});
+			$('#switchBtn').on('click',function(){
+				alert('스위치 수열 클릭!');
+				$('#content').empty();
+				$('#content').append();
+			});
+			$('#geoBtn').on('click',function(){
+				alert('geometric 클릭!');
+				$('#content').empty();
+			});
+			$('#facBtn').on('click',function(){
+				alert('factorial 클릭!');
+				$('#content').empty();
+			});
+			$('#fiboBtn').on('click',function(){
+				alert('fibonacci 클릭!');
+				$('#content').empty();
+			});
 		});
 	};
 	/*속성*/
@@ -62,10 +86,9 @@ meta.index=(function(){   /*생성자*/
 			  src: img+'/loading.gif'
 		    }
 		);
-		
-		$image.appendTo($wrapper);
-		$('#wrapper').empty();
-		
+		$wrapper.append($image);
+	
+	
 		var $btn=$('<input/>',
 				{
 					id:'button',
@@ -73,6 +96,7 @@ meta.index=(function(){   /*생성자*/
 					value:'click!'
 				}
 		);
+
 		$btn.appendTo($wrapper);
 		//$('#wrapper').append(button);
 		
@@ -148,12 +172,19 @@ meta.auth=(function(){
  * meta.algo*/
 
 meta.algo = {
-	/*1부터 100까지 등차수열의 합 */
 
- arithmetic: function(){
-	 var sum=0;
-
- }
+	 arithmetic: function(s,e){
+		 var sum=0;
+		 var start= s*1;   /*parameter를 인자값으로 바꾸는것 */
+		 var end = e*1;
+		 for(var i=start;i<=end;i++){
+			 sum+=i;
+		 }
+		 return sum;
+	 },
+	 swutcgSerues : function(){
+		 /*스위치 함수*/
+	 }
 };
 
 
@@ -165,34 +196,131 @@ meta.ui=(function(){
 	var $wrapper,img,ctx,js,css;
 	var init = function(){
 		$wrapper = $('#wrapper');
+		img=$$('i');
 	};
 	var arithmetic=function(){
-
-		 var ui= '<div id="ui">'
-			 +'<h1>arithmetic   등차수열의 합</h1>'
-		 +'<span id="startVal">시작 값 : </span>'
-		 +'</br> <span id="endVal">끝 값: </span>'
-		 +'</br> <span id="result" style="red">result: </span>'
-		 +'<button id="result" value="결과보기"/>'
-		 +'<span>result: </span> </div>';
+		 var content= '<div id="content">'
+		 +'<h1>1부터 100까지 등차수열의 합</h1>'
+		 +'</br>'
+		 +'<span id="start_txt">시작 값 : </span>'
+		 +'</br>'
+		 +'<span id="end_txt">끝 값: </span>'
+		 +'</br>'
+		 +'<div id="result_btn"></div>'
+		 +'</div>';
+		 $wrapper.after(content);
+		 $('#start_txt').after(meta.comp.input(
+			 {
+				 type : 'text',
+				 id :'start',
+				 placeholder : '시작 값 '
+		     }
+		 ));
+		 $('#end_txt').after(meta.comp.input(
+				 {
+					 type : 'text',
+					 id : 'end',
+					 placeholder : '끝 값'
+				 }
+		 ));
+		 $('#result_btn').before(meta.comp.input(
+				 {
+					 type : 'button',
+					 id :'result',
+					 value :'결과 보기!'
+				 }
+		 ));
 	};
+	
+	var switchSeries= function(){
+		'<div id="ui">'
+		 +'<h1>switch수열의 합</h1>'
+	 +'<span id="startVal">시작 값 : </span>'
+	 +'</br> <span id="endVal">끝 값: </span>'
+	 +'</div>';
+	};
+
+	var navbar=function(){
+		$wrapper.html(
+				'<nav class="navbar navbar-inverse">'
+					 +'<div class="container-fluid">'
+					    +'<div class="navbar-header">'
+					      +'<a class="navbar-brand" href="#">GMS</a>'
+					    +'</div>'
+					    +'<ul class="nav navbar-nav">'
+					      +'<li class="active"><a id="homeBtn" ><span class="glyphicon glyphicon-home"> &nbsp;HOME</span></a></li>'
+					      +'<li class="dropdown">'
+					      +'<a href="" class="dropdown-toggle" data-toggle="dropdown" role="button" aria-haspopup="true" aria-expanded="flase">학생관리 </a>' 
+					       +'<ul id="navbar_ul_stu" class="dropdown-menu">'
+					       	+'<li class="dropdown-menu"><a>학생추가</a></li>'
+					       	+'<li><a>학생 목록</a></li>'
+					       	+'<li><a>학생 조회</a></li>'
+					       	+'<li ></li>'
+					       	+'<li><a>학생삭제</a></li>'
+					       +'</ul>'
+					      +'</li>'
+					       +'<li class="dropdown">'
+					      +'<a href="" class="dropdown-toggle" data-toggle="dropdown" role="button" aria-haspopup="true" aria-expanded="flase">성적관리 </a>'
+					     +'<ul id="navbar_ul_grade"  class="dropdown-menu">'
+					       	+'<li><a>성적추가</a></li>'
+					       	+'<li><a>성적 목록</a></li>'
+					       	+'<li><a>성적 조회</a></li>'
+					       	+'<li role="separator" class="divider"></li>'
+					       	+'<li><a class="dropdown-menu a">성적 삭제</a></li>'
+					       +'</ul>'
+					      +'</li>'
+					     +'<li class="dropdown">'
+					      +'<a href="" class="dropdown-toggle" data-toggle="dropdown" role="button" aria-haspopup="true" aria-expanded="flase">게시판 관리 </a>'
+					      +'<ul id="navbar_ul_board"  class="dropdown-menu">'
+					       	+'<li><a>게시판 추가</a></li>'
+					       	+'<li><a>게시판 목록</a></li>'
+					       	+'<li><a>게시판 조회</a></li>'
+					       	+'</ul>'
+						  +'</li>'
+					        +'<li class="dropdown">'
+						   +'<a href="" class="dropdown-toggle" data-toggle="dropdown" role="button" aria-haspopup="true" aria-expanded="flase">수  열 </a>'
+						    +'<ul id="navbar_ul_series"  class="dropdown-menu">'
+						       	+'<li><a id="arithBtn">등차 수열</a></li>'
+						       	+'<li><a id="switchBtn">스위치 수열</a></li>'
+						       	+'<li><a id="geoBtn">등비 수열 </a></li>'
+						       	+'<li><a id="facBtn">팩토리얼 </a></li>'
+						       	+'<li><a id="fiboBtn">피보나치 </a></li>'
+					       +'</ul>'
+					     +' </li>'
+					    +'</ul>'
+					    +'<ul class="nav navbar-nav navbar-right">'
+					      +'<li style="font-weight:bold;color: white ">${user.name}</li>'
+					      +'<li><a id="logout" ><span class="glyphicon glyphicon-user"></span>Logout</a></li>'
+					     +' <li><a href="#"><span class="glyphicon glyphicon-log-in"></span> Login</a></li>'
+					    +'</ul>'
+					 +' </div>'
+					+'</nav>'
+			);
+	};
+	
 	return {
-		arithmetic:arithmetic
+		init:init,
+		arithmetic:arithmetic,
+		switchSeries:switchSeries,
+		navbar : navbar
 	};
 })();
+
+
 
 /*
  * meta.component
  * 부품을 만드는것*/
-meta.comp= /*객체 literal 생성방식*/
-
-	{
+/*객체 literal 생성방식*/
+meta.comp= {
 		button:function(json){
 			return $('<input/>',json);
 			//'<input type="button" id="'+ i +'" value="'+ v +'"/>';
+		},
+		input : function(json){
+			return $('<input/>',json);
 		}
-	};
-
+};
 
 
 
